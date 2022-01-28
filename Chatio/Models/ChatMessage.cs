52902f56ff1662a3db159c0b8782be5fd1ce9cad
bartdeveloper <bartek@appselio.pl>
2022-01-28 18:18:55 +0100

    Implemented SignalR

---
+++ Chatio/Models/ChatMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Chatio.Models
+{
+    public class ChatMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public bool CurrentUser { get; set; }
+        public DateTime DateSent { get; set; }
+    }
+}
--- Chatio/Program.cs
+++ Chatio/Program.cs
@@ -1,6 +1,8 @@
 using Chatio.Data;
+using Chatio.Hubs;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddResponseCompression(opts => {
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
+});
 
 var app = builder.Build();
 
@@ -26,6 +31,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+app.MapHub<ChatHub>("/chathub");
 app.MapFallbackToPage("/_Host");
 
 app.Run();
