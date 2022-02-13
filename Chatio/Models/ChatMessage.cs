e3a9b501997183bec45c13da85f736accdd32e6f
bartdeveloper <bartek@appselio.pl>
2022-02-13 10:16:39 +0100

    Added toasts and getting local user time zone

--- Chatio/Models/ChatMessage.cs
+++ Chatio/Models/ChatMessage.cs
@@ -5,6 +5,6 @@
         public string Username { get; set; }
         public string Message { get; set; }
         public bool CurrentUser { get; set; }
-        public DateTime DateSent { get; set; }
+        public DateTimeOffset DateSent { get; set; }
     }
 }
