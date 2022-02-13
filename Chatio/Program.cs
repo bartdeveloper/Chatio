ef36b1ff04b2fadd5fef4ae2f557c483992ca589
bartdeveloper <bartek@appselio.pl>
2022-02-13 10:14:05 +0100

    Added time zone service and local storage package

--- Chatio/Program.cs
+++ Chatio/Program.cs
@@ -1,18 +1,29 @@
+using Blazored.LocalStorage;
 using Blazored.Toast.Services;
 using Chatio.Hubs;
 using Chatio.Models;
+using Chatio.Services;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddServerSideBlazor(options =>
+{
+    options.DetailedErrors = true;
+    options.DisconnectedCircuitMaxRetained = 100;
+    options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(60);
+    options.JSInteropDefaultCallTimeout = TimeSpan.FromMinutes(1);
+    options.MaxBufferedUnacknowledgedRenderBatches = 10;
+});
 builder.Services.AddResponseCompression(opts => {
     opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
 });
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddSingleton<IDictionary<string, ChatUser>>(opts => new Dictionary<string, ChatUser>());
 builder.Services.AddScoped<IToastService, ToastService>();
+builder.Services.AddScoped<TimeZoneService>();
 
 var app = builder.Build();
 
