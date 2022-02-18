2e02505593072d4ba4acc0a8cb37be16eefb8312
bartdeveloper <bartek@appselio.pl>
2022-02-18 13:34:52 +0100

    Added time format service

---
+++ Chatio.Server/Services/TimeFormatService.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Chatio.Server.Services
+{
+    public static class TimeFormatService
+    {
+        public static string ToPolandFormat(this DateTimeOffset dateTimeOffset)
+        {
+            return dateTimeOffset.ToString("HH:mm | dd MMMM yyy", new CultureInfo("pl-PL"));
+        }
+    }
+}
